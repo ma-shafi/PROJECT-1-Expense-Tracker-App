aee4966257a27e13b368bad0df58024fddefff0c
mashafi <mashafi.tech@gmail.com>
2021-04-27 11:26:28 +0500

    updated action build with sudo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
  import './App.css';
+ import {Child} from './Child'
+// import components
+/*import {Header} from './components/Header'
+import {Balance} from './components/Balance'
+import {AccountSummary} from './AccountSummary'
+import {History} from './components/History'*/
+
+
 
 function App() {
   return (
-    <div>
-      Hello New World!!
+    <div className="container">
+      <Child />
     </div>
   );
 }
